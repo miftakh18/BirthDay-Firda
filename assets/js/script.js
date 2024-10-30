b99935d644b9921997fa0dc3050e519548974edb
miftakh18 <mmiftakh18@gmail.com>
2024-10-31 00:41:34 +0700

    tahap animasi

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+// Mendapatkan elemen
+const element = document.getElementById("typing");
+const text = element.innerHTML; // Mendapatkan teks asli
+element.innerHTML = ""; // Mengosongkan konten untuk efek mengetik
+
+let i = 0;
+
+// Fungsi untuk efek mengetik
+function type() {
+  if (i < text.length) {
+    element.innerHTML += text.charAt(i);
+    i++;
+    setTimeout(type, 100); // Waktu antara setiap karakter
+  }
+}
+
+// Mulai animasi mengetik
+
 function modalCustom(id, body, btn) {
   return `<div class="modal fade custom-zoom" id="${id}" aria-labelledby="modalLabel" aria-hidden="true" >
                 <div class="modal-dialog modal-dialog-centered">
@@ -9,6 +27,10 @@ function modalCustom(id, body, btn) {
                     </div>
                 </div>
             </div>`;
+}
+
+function content2() {
+  return ` `;
 }
 
 function tampilkanTanggalIndonesia() {
@@ -97,6 +119,7 @@ function hitungUmur(tanggalLahir) {
 }
 
 const clickSound = document.getElementById("click-sound");
+const clickSound2 = document.getElementById("click-sound2");
 const data = [
   {
     body: "Hi Sayang1",
@@ -147,19 +170,43 @@ const data = [
     },
   },
 ];
+
+element.innerHTML = ""; // Mengosongkan teks di awal
+
+// Menambahkan kelas slide-down
+element.classList.add("slide-down");
+function type() {
+  if (i < text.length) {
+    if (text.charAt(i) === "\n") {
+      element.innerHTML += "<br>";
+    } else {
+      element.innerHTML += text.charAt(i);
+    }
+    i++;
+    setTimeout(type, 50); // Kecepatan mengetik
+  } else {
+    $("#intipBuka").show();
+  }
+}
+
 $(function () {
+  $("#intipBuka").hide();
   let modal = ``;
   setInterval(tampilkanJam, 1000);
   setInterval(tampilkanTanggalIndonesia, 1000);
+
+  // setTimeout(() => {
+  // }, 101);
   for (let i = 0; i < data.length; i++) {
     modal += modalCustom(`mod_${i}`, `${data[i].body}`, data[i].button(i));
   }
-  $("#content-2").hide();
+
   let tglLahir = "2001-10-19";
   let umur = hitungUmur(tglLahir);
-
-  $("#umur").html(`Selamat Umur ${umur} Tahun`);
   $("#content").html(modal);
+  $("#content-2").hide();
+  $("#notifikasi").hide();
+  $("#umur").html(`Selamat Umur ${umur} Tahun`);
   for (let i = 0; i < data.length; i++) {
     if (i === 0) {
       $(`#mod_${i}`).modal({
@@ -189,6 +236,7 @@ $(function () {
         $(`#btn${i + 1}`).attr("data-backdrop", `static`);
         $(`#btn${i + 1}`).attr("data-keyboard", `false`);
         $("#content-2").show();
+        type();
       });
     } else {
       $("#content").on("click", `#btn${i}`, function (event) {
@@ -206,4 +254,27 @@ $(function () {
       });
     }
   }
+});
+function slideUpAndHide() {
+  const box = document.getElementById("halaman_awal");
+  box.classList.add("slide-up");
+
+  // Menghapus elemen setelah animasi selesai
+  box.addEventListener("transitionend", () => {
+    box.style.display = "none";
+  });
+}
+
+$(document).on("click", "#intipBuka", function () {
+  slideUpAndHide();
+  $("#notifikasi-box").addClass("slide-left");
+
+  setTimeout(() => {
+    $("#notifikasi").show();
+
+    $("#notifikasi-box").addClass("active");
+  }, 1000);
+  $("#notifikasi-body").html(`From Your Love`);
+
+  clickSound2.play();
 });
