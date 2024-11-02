e88fd3e3c2f67e01d86bbeabd08aeda8c438aa75
miftakh18 <mmiftakh18@gmail.com>
2024-11-02 23:45:53 +0700

    tahap custom

---
+++ assets/js/function.js
@@ -0,0 +1,147 @@
+function modalCustom(id, body, btn) {
+  return `<div class="modal fade custom-zoom" id="${id}" aria-labelledby="modalLabel" aria-hidden="true" >
+                  <div class="modal-dialog modal-dialog-centered">
+                      <div class="modal-content" style="background-color: #333; color: #fff">
+                          <div  class="modal-body fw-bold d-flex d-flex justify-content-center p-5">${body}</div>
+                          <div class="modal-footer">
+                             ${btn}
+                          </div>
+                      </div>
+                  </div>
+              </div>`;
+}
+
+function tampilkanTanggalIndonesia() {
+  // Dapatkan waktu saat ini
+  const sekarang = new Date();
+
+  // Daftar nama hari dan bulan dalam bahasa Indonesia
+  const hariIndo = [
+    "Minggu",
+    "Senin",
+    "Selasa",
+    "Rabu",
+    "Kamis",
+    "Jumat",
+    "Sabtu",
+  ];
+  const bulanIndo = [
+    "Januari",
+    "Februari",
+    "Maret",
+    "April",
+    "Mei",
+    "Juni",
+    "Juli",
+    "Agustus",
+    "September",
+    "Oktober",
+    "November",
+    "Desember",
+  ];
+
+  // Ambil hari, tanggal, bulan, dan tahun
+  const hari = hariIndo[sekarang.getDay()];
+  const tanggal = sekarang.getDate();
+  const bulan = bulanIndo[sekarang.getMonth()];
+  const tahun = sekarang.getFullYear();
+
+  // Formatkan tanggal sesuai format Indonesia
+  const tanggalIndonesia = `${hari}, ${tanggal} ${bulan} ${tahun}`;
+
+  // Tampilkan tanggal di elemen dengan id 'tanggal'
+  $("#tanggal").html(tanggalIndonesia);
+}
+
+function tampilkanJam() {
+  // Dapatkan waktu saat ini
+  const sekarang = new Date();
+
+  // Ambil jam, menit, dan detik saat ini
+  let jam = sekarang.getHours();
+  let menit = sekarang.getMinutes();
+  let detik = sekarang.getSeconds();
+
+  // Tambahkan '0' jika jam, menit, atau detik di bawah 10
+  jam = jam < 10 ? "0" + jam : jam;
+  menit = menit < 10 ? "0" + menit : menit;
+  detik = detik < 10 ? "0" + detik : detik;
+
+  // Gabungkan jam, menit, dan detik
+  const waktu = `${jam}:${menit}`;
+
+  // Tampilkan waktu di elemen dengan id 'jam'
+  $("#jam").html(waktu);
+}
+
+function hitungUmur(tanggalLahir) {
+  // Ubah tanggal lahir menjadi objek Date
+  const birthDate = new Date(tanggalLahir);
+
+  // Ambil tanggal hari ini
+  const today = new Date();
+
+  // Hitung selisih tahun
+  let age = today.getFullYear() - birthDate.getFullYear();
+
+  // Periksa apakah bulan dan hari ini belum melewati ulang tahun tahun ini
+  const monthDiff = today.getMonth() - birthDate.getMonth();
+  const dayDiff = today.getDate() - birthDate.getDate();
+
+  // Jika bulan lebih kecil, atau bulan sama tetapi hari lebih kecil, kurangi umur
+  if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+    age--;
+  }
+
+  return age;
+}
+
+function type() {
+  if (i < text.length) {
+    element.innerHTML += text.charAt(i);
+    i++;
+    setTimeout(type, 100); // Waktu antara setiap karakter
+  }
+}
+
+function type() {
+  if (i < text.length) {
+    if (text.charAt(i) === "\n") {
+      element.innerHTML += "<br>";
+    } else {
+      element.innerHTML += text.charAt(i);
+    }
+    i++;
+    setTimeout(type, 50); // Kecepatan mengetik
+  } else {
+    $("#intipBuka").show();
+  }
+}
+
+function slideUpAndHide() {
+  const box = document.getElementById("halaman_awal");
+  box.classList.add("slide-up");
+
+  // Menghapus elemen setelah animasi selesai
+  box.addEventListener("transitionend", () => {
+    box.style.display = "none";
+  });
+}
+
+function buttonsFirst(
+  id,
+  TextOk = null,
+  bottonCancel = false,
+  TextCancel = null
+) {
+  let button = `<button id="btnOK${id}" type="button"  class=" btn-none w-100 text-primary fw-bold">${
+    TextOk !== null ? TextOk : "Ok"
+  }</button>`;
+
+  if (bottonCancel == true) {
+    button += `<button id="btnCancel${id}" type="button"  class=" btn-none w-100 text-primary fw-bold">${
+      TextCancel !== null ? TextCancel : "Cancel"
+    }</button>`;
+  }
+  return button;
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -6,191 +6,20 @@ element.innerHTML = ""; // Mengosongkan konten untuk efek mengetik
 let i = 0;
 
 // Fungsi untuk efek mengetik
-function type() {
-  if (i < text.length) {
-    element.innerHTML += text.charAt(i);
-    i++;
-    setTimeout(type, 100); // Waktu antara setiap karakter
-  }
-}
 
 // Mulai animasi mengetik
 
-function modalCustom(id, body, btn) {
-  return `<div class="modal fade custom-zoom" id="${id}" aria-labelledby="modalLabel" aria-hidden="true" >
-                <div class="modal-dialog modal-dialog-centered">
-                    <div class="modal-content" style="background-color: #333; color: #fff">
-                        <div  class="modal-body fw-bold d-flex d-flex justify-content-center p-5">${body}</div>
-                        <div class="modal-footer">
-                           ${btn}
-                        </div>
-                    </div>
-                </div>
-            </div>`;
-}
-
-function content2() {
-  return ` `;
-}
-
-function tampilkanTanggalIndonesia() {
-  // Dapatkan waktu saat ini
-  const sekarang = new Date();
-
-  // Daftar nama hari dan bulan dalam bahasa Indonesia
-  const hariIndo = [
-    "Minggu",
-    "Senin",
-    "Selasa",
-    "Rabu",
-    "Kamis",
-    "Jumat",
-    "Sabtu",
-  ];
-  const bulanIndo = [
-    "Januari",
-    "Februari",
-    "Maret",
-    "April",
-    "Mei",
-    "Juni",
-    "Juli",
-    "Agustus",
-    "September",
-    "Oktober",
-    "November",
-    "Desember",
-  ];
-
-  // Ambil hari, tanggal, bulan, dan tahun
-  const hari = hariIndo[sekarang.getDay()];
-  const tanggal = sekarang.getDate();
-  const bulan = bulanIndo[sekarang.getMonth()];
-  const tahun = sekarang.getFullYear();
-
-  // Formatkan tanggal sesuai format Indonesia
-  const tanggalIndonesia = `${hari}, ${tanggal} ${bulan} ${tahun}`;
-
-  // Tampilkan tanggal di elemen dengan id 'tanggal'
-  $("#tanggal").html(tanggalIndonesia);
-}
-
-function tampilkanJam() {
-  // Dapatkan waktu saat ini
-  const sekarang = new Date();
-
-  // Ambil jam, menit, dan detik saat ini
-  let jam = sekarang.getHours();
-  let menit = sekarang.getMinutes();
-  let detik = sekarang.getSeconds();
-
-  // Tambahkan '0' jika jam, menit, atau detik di bawah 10
-  jam = jam < 10 ? "0" + jam : jam;
-  menit = menit < 10 ? "0" + menit : menit;
-  detik = detik < 10 ? "0" + detik : detik;
-
-  // Gabungkan jam, menit, dan detik
-  const waktu = `${jam}:${menit}`;
-
-  // Tampilkan waktu di elemen dengan id 'jam'
-  $("#jam").html(waktu);
-}
-
-function hitungUmur(tanggalLahir) {
-  // Ubah tanggal lahir menjadi objek Date
-  const birthDate = new Date(tanggalLahir);
-
-  // Ambil tanggal hari ini
-  const today = new Date();
-
-  // Hitung selisih tahun
-  let age = today.getFullYear() - birthDate.getFullYear();
-
-  // Periksa apakah bulan dan hari ini belum melewati ulang tahun tahun ini
-  const monthDiff = today.getMonth() - birthDate.getMonth();
-  const dayDiff = today.getDate() - birthDate.getDate();
-
-  // Jika bulan lebih kecil, atau bulan sama tetapi hari lebih kecil, kurangi umur
-  if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
-    age--;
-  }
-
-  return age;
-}
-
 const clickSound = document.getElementById("click-sound");
 const clickSound2 = document.getElementById("click-sound2");
-const data = [
-  {
-    body: "Hi Sayang1",
-
-    button: function (id) {
-      return `<button id="btn${id}" type="button"  class=" btn-none w-100 text-primary fw-bold"  >OK</button>`;
-    },
-  },
-  {
-    body: "Hi Sayang2",
-
-    button: function (id) {
-      return `<button id="btn${id}" type="button" class=" btn-none w-100 text-primary fw-bold"  >OK</button>`;
-    },
-  },
-  {
-    body: "Hi Sayang3",
-    button: function (id) {
-      return `<button id="btn${id}" type="button" class=" btn-none w-100 text-primary fw-bold"  >OK</button>`;
-    },
-  },
-  {
-    body: "Hi Sayang4",
-
-    button: function (id) {
-      return `<button id="btn${id}" type="button" class=" btn-none w-100 text-primary fw-bold" >OK</button>`;
-    },
-  },
-  {
-    body: "Hi Sayang5",
-
-    button: function (id) {
-      return `<button id="btn${id}" type="button" class=" btn-none w-100 text-primary fw-bold"  >OK</button>`;
-    },
-  },
-  {
-    body: "Hi Sayang6",
-
-    button: function (id) {
-      return `<button id="btn${id}" type="button" class=" btn-none w-100 text-primary fw-bold"  >OK</button>`;
-    },
-  },
-  {
-    body: "Hi Sayang7",
-
-    button: function (id) {
-      return `<button id="btn${id}" type="button" class=" bg-none btn-none w-100 text-primary fw-bold"  >OK</button>`;
-    },
-  },
-];
 
 element.innerHTML = ""; // Mengosongkan teks di awal
-
+const data = ["Aku sayang kamu", "Sedang apa sayang"];
 // Menambahkan kelas slide-down
 element.classList.add("slide-down");
-function type() {
-  if (i < text.length) {
-    if (text.charAt(i) === "\n") {
-      element.innerHTML += "<br>";
-    } else {
-      element.innerHTML += text.charAt(i);
-    }
-    i++;
-    setTimeout(type, 50); // Kecepatan mengetik
-  } else {
-    $("#intipBuka").show();
-  }
-}
 
 $(function () {
   $("#intipBuka").hide();
+  $("#btnNext").hide();
   let modal = ``;
   setInterval(tampilkanJam, 1000);
   setInterval(tampilkanTanggalIndonesia, 1000);
@@ -198,11 +27,10 @@ $(function () {
   // setTimeout(() => {
   // }, 101);
   for (let i = 0; i < data.length; i++) {
-    modal += modalCustom(`mod_${i}`, `${data[i].body}`, data[i].button(i));
+    modal += modalCustom(`mod_${i}`, `${data[i]}`, buttonsFirst(i, "Mau"));
   }
 
-  let tglLahir = "2001-10-19";
-  let umur = hitungUmur(tglLahir);
+  let umur = hitungUmur("2001-10-19");
   $("#content").html(modal);
   $("#content-2").hide();
   $("#notifikasi").hide();
@@ -214,38 +42,42 @@ $(function () {
         keyboard: false,
       });
       $(`#mod_${i}`).modal("show");
-      $("#content").on("click", `#btn${i}`, function (event) {
+      $("#content").on("click", `#btnOK${i}`, function (event) {
         clickSound.play();
         $(`#mod_${i}`).modal("hide");
-        $(`#btn${i + 1}`).attr("data-target", `#mod_${i + 1}`);
-        $(`#btn${i + 1}`).attr("data-toggle", `modal`);
-        $(`#btn${i + 1}`).attr("data-backdrop", `static`);
-        $(`#btn${i + 1}`).attr("data-keyboard", `false`);
+        $(`#btnOK${i + 1}`).attr("data-target", `#mod_${i + 1}`);
+        $(`#btnOK${i + 1}`).attr("data-toggle", `modal`);
+        $(`#btnOK${i + 1}`).attr("data-backdrop", `static`);
+        $(`#btnOK${i + 1}`).attr("data-keyboard", `false`);
         $(`#mod_${i + 1}`).modal({
           backdrop: "static",
           keyboard: false,
         });
         $(`#mod_${i + 1}`).modal("show");
+        if (i === data.length - 1) {
+          $("#content-2").show();
+          type();
+        }
       });
     } else if (i === data.length - 1) {
-      $("#content").on("click", `#btn${i}`, function (event) {
+      $("#content").on("click", `#btnOK${i}`, function (event) {
         clickSound.play();
         $(`#mod_${i}`).modal("hide");
-        $(`#btn${i + 1}`).attr("data-target", `#mod_${i + 1}`);
-        $(`#btn${i + 1}`).attr("data-toggle", `modal`);
-        $(`#btn${i + 1}`).attr("data-backdrop", `static`);
-        $(`#btn${i + 1}`).attr("data-keyboard", `false`);
+        $(`#btnOK${i + 1}`).attr("data-target", `#mod_${i + 1}`);
+        $(`#btnOK${i + 1}`).attr("data-toggle", `modal`);
+        $(`#btnOK${i + 1}`).attr("data-backdrop", `static`);
+        $(`#btnOK${i + 1}`).attr("data-keyboard", `false`);
         $("#content-2").show();
         type();
       });
     } else {
-      $("#content").on("click", `#btn${i}`, function (event) {
+      $("#content").on("click", `#btnOK${i}`, function (event) {
         clickSound.play();
         $(`#mod_${i}`).modal("hide");
-        $(`#btn${i + 1}`).attr("data-target", `#mod_${i + 1}`);
-        $(`#btn${i + 1}`).attr("data-toggle", `modal`);
-        $(`#btn${i + 1}`).attr("data-backdrop", `static`);
-        $(`#btn${i + 1}`).attr("data-keyboard", `false`);
+        $(`#btnOK${i + 1}`).attr("data-target", `#mod_${i + 1}`);
+        $(`#btnOK${i + 1}`).attr("data-toggle", `modal`);
+        $(`#btnOK${i + 1}`).attr("data-backdrop", `static`);
+        $(`#btnOK${i + 1}`).attr("data-keyboard", `false`);
         $(`#mod_${i + 1}`).modal({
           backdrop: "static",
           keyboard: false,
@@ -255,26 +87,38 @@ $(function () {
     }
   }
 });
-function slideUpAndHide() {
-  const box = document.getElementById("halaman_awal");
-  box.classList.add("slide-up");
 
-  // Menghapus elemen setelah animasi selesai
-  box.addEventListener("transitionend", () => {
-    box.style.display = "none";
-  });
-}
-
+let content_part2 = ["tes Konten", "Konten 2"];
 $(document).on("click", "#intipBuka", function () {
   slideUpAndHide();
+  let id = $(this).data("id");
+  $("#btnNext").hide();
+
+  $("#notifikasi-box").removeClass("active");
+
   $("#notifikasi-box").addClass("slide-left");
 
   setTimeout(() => {
     $("#notifikasi").show();
+    $("#btnNext").show();
 
     $("#notifikasi-box").addClass("active");
+    let btnId = id + 1;
+    if (typeof id === "undefined") {
+      $("#btnNext").html(``);
+    } else {
+      console.log(id);
+      $("#btnNext").html(`<button type="button" id="intipBuka" ${
+        id !== content_part2.length - 1 ? "data-id='" + btnId + "'" : ""
+      } class="btn btn-outline-light slide-down shadow-lg rounded-custom"> Intip Jangan ?&#128140;
+          </button>`);
+    }
   }, 1000);
-  $("#notifikasi-body").html(`From Your Love`);
+  if (typeof id === "undefined") {
+    $("#notifikasi").html("");
+  } else {
+    $("#notifikasi-body").html(content_part2[id]);
+  }
 
   clickSound2.play();
 });
