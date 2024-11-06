cd728807d83c58410156bd9155ebcb76a3bdc784
miftakh18 <mmiftakh18@gmail.com>
2024-11-06 23:10:28 +0700

    + sw2

--- assets/js/script.js
+++ assets/js/script.js
@@ -116,6 +116,43 @@ $(document).on("click", "#intipBuka", function () {
   }, 1000);
   if (typeof id === "undefined") {
     $("#notifikasi").html("");
+    Swal.fire({
+      title: "Submit your Github username",
+      input: "text",
+      inputAttributes: {
+        autocapitalize: "off",
+      },
+      showCancelButton: true,
+      allowOutsideClick: false,
+      confirmButtonText: "Look up",
+      showLoaderOnConfirm: true,
+      preConfirm: async (login) => {
+        try {
+          const githubUrl = `
+            https://api.github.com/users/${login}
+          `;
+          const response = await fetch(githubUrl);
+          if (!response.ok) {
+            return Swal.showValidationMessage(`
+              ${JSON.stringify(await response.json())}
+            `);
+          }
+          return response.json();
+        } catch (error) {
+          Swal.showValidationMessage(`
+            Request failed: ${error}
+          `);
+        }
+      },
+      allowOutsideClick: () => !Swal.isLoading(),
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: `${result.value.login}'s avatar`,
+          imageUrl: result.value.avatar_url,
+        });
+      }
+    });
   } else {
     $("#notifikasi-body").html(content_part2[id]);
   }
