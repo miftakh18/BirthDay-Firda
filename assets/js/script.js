a03adbb04ae41d0fcfeef31ad9ff27bf5ac69dda
miftakh18 <mmiftakh18@gmail.com>
2024-11-11 01:07:30 +0700

    finising

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,17 +4,27 @@ const text = element.innerHTML; // Mendapatkan teks asli
 element.innerHTML = ""; // Mengosongkan konten untuk efek mengetik
 
 let i = 0;
-
+window.addEventListener("scroll", () => {
+  document.querySelector(
+    ".background"
+  ).style.transform = `translateY(${window.scrollY}px)`;
+});
 // Fungsi untuk efek mengetik
 
 // Mulai animasi mengetik
 
 const clickSound = document.getElementById("click-sound");
 const clickSound2 = document.getElementById("click-sound2");
+const clickSound3 = document.getElementById("click-sound3");
 
 element.innerHTML = ""; // Mengosongkan teks di awal
-const data = ["Aku sayang kamu", "Sedang apa sayang"];
-// Menambahkan kelas slide-down
+const data = [
+  "Hii, sayang &#128513;",
+  "pak tarmizi anak mu sudah dewasa &#128522;",
+  "ada pesan untuk kamu di baca yaa",
+];
+
+/// Menambahkan kelas slide-down
 element.classList.add("slide-down");
 
 $(function () {
@@ -27,7 +37,7 @@ $(function () {
   // setTimeout(() => {
   // }, 101);
   for (let i = 0; i < data.length; i++) {
-    modal += modalCustom(`mod_${i}`, `${data[i]}`, buttonsFirst(i, "Mau"));
+    modal += modalCustom(`mod_${i}`, `${data[i]}`, buttonsFirst(i, "Lanjut"));
   }
 
   let umur = hitungUmur("2001-10-19");
@@ -62,6 +72,8 @@ $(function () {
     } else if (i === data.length - 1) {
       $("#content").on("click", `#btnOK${i}`, function (event) {
         clickSound.play();
+        clickSound3.play();
+
         $(`#mod_${i}`).modal("hide");
         $(`#btnOK${i + 1}`).attr("data-target", `#mod_${i + 1}`);
         $(`#btnOK${i + 1}`).attr("data-toggle", `modal`);
@@ -88,7 +100,26 @@ $(function () {
   }
 });
 
-let content_part2 = ["tes Konten", "Konten 2"];
+let content_part2 = [
+  `Hari istimewa itu terlewatkan
+   Meski waktu tak kembali lagiiiiii....
+   Wanita tersayangkuuu kau istimewaaaa &#128522;<br>
+   Tiada hiasan atau lilin terang.  <br>
+   Namun doa dan harapan menyertai.<br>
+Semoga kebahagiaan selalu datang,<br>
+Dalam setiap langkah yang kau jalani.&#128521;`,
+  `<b>Dear Kamu</b>,<br> seorang wanita yang tak pernah ku temui <br>
+   setiap text yang kamu sampaikan membuatku merasa tidak sendiri
+   <br>Di balik layar ini kita bertukar rasa .<br> Dalam setiap pesan, kamu hadir nyata,
+   <br> Semoga suatu hari kita bertemu,Dalam pelukan nyata yang kurindu
+   `,
+  `Aku merindukanmu, lebih dari jejak yang kau tinggalkan, karena di balik sepiku yang dalam, <b>Kamu Tak Hilang</b>`,
+  `terakhir untuk kamu , aku menemukan sajak dari seorang pemuda yang pandai membuat setiap baitnya mampu mengingat mu,dalam kutipan sajak nya <br>
+  "Jika, Suatu saat kau rasa <br> Dunia tak begitu baik kepada mu <br> Maka <br> Biarkan aku memelukmu Ceritakan segala keluhmu<br> agar kau tahu masih ada Dunia bernama <br> <i><b>"Aku"</b></i> " Hadiah ku Untuk mu `,
+  `<b>Dear sayang </b> <br> dalam dua dimensi aku terlarut dalam lekukan garis senyum mu,Dalam lekukan bibir itu aku menemukan secarik kebahagian`,
+  `
+  <i>"Sayang terima kasih untuk terus bertahan sampai hari ini "</i>`,
+];
 let textBtn = "Selanjutnya ";
 $(document).on("click", "#intipBuka", function () {
   slideUpAndHide();
@@ -117,28 +148,34 @@ $(document).on("click", "#intipBuka", function () {
   }, 1000);
   if (typeof id === "undefined") {
     $("#notifikasi").html("");
-    Swal.fire({
-      title: "Mungkin ada yang mau di sampaikan",
-      input: "textarea",
-      inputAttributes: {
-        autocapitalize: "off",
+    Swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-success",
       },
-      showCancelButton: false,
-      allowOutsideClick: false,
-      confirmButtonText: "Kirim ke whatsapp",
-      showLoaderOnConfirm: true,
-      preConfirm: async (send) => {
-        const data = {
-          text: send,
-          no_hp: "6281299700738",
-        };
-        return data;
-      },
-    }).then((result) => {
-      if (result.isConfirmed) {
-        window.location.href = `https://wa.me/${result.value.no_hp}?text=${result.value.text}`;
-      }
-    });
+    })
+      .fire({
+        title: "Mungkin ada yang mau di sampaikan",
+        input: "textarea",
+        inputAttributes: {
+          autocapitalize: "off",
+        },
+        showCancelButton: false,
+        allowOutsideClick: false,
+        confirmButtonText: "Kirim ke dia",
+        showLoaderOnConfirm: true,
+        preConfirm: async (send) => {
+          const data = {
+            text: send,
+            no_hp: "6281299700738",
+          };
+          return data;
+        },
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          window.location.href = `https://wa.me/${result.value.no_hp}?text=${result.value.text}`;
+        }
+      });
   } else {
     $("#notifikasi-body").html(content_part2[id]);
   }
